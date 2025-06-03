bcc959e8bca700e8f55b234bf073eff6360e23cd
Nicholas Nathanael Wijaya <nicholasnathanael098@gmail.com>
2025-06-03 15:08:46 +0700

    card

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Hero from "@/components/hero";
+import Main from "@/components/main";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <div className="mt-16">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold uppercase">Room & Rates</h1>
+          <p className="py-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error at commodi ratione ab doloribus impedit.
+          </p>
+        </div>
+        <Main />
+      </div>
     </div>
   );
 }
